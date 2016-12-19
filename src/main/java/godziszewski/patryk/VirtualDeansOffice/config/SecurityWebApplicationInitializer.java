48aad93ca77c40945b6bcccb081f2fe6c344c6e5
pgod <p.godziszewski@o2.pl>
2016-12-19 10:21:07 +0100

    Added spring security to project

---
+++ src/main/java/godziszewski/patryk/VirtualDeansOffice/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package godziszewski.patryk.VirtualDeansOffice.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by Patryk on 2016-12-19.
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/java/godziszewski/patryk/VirtualDeansOffice/config/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package godziszewski.patryk.VirtualDeansOffice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Patryk on 2016-12-19.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password("password")
+                .roles("STUDENT");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure((http));
+    }
+}
